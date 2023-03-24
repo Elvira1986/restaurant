0d82398e480306b3376585b59f4a7290bd317b22
Elvira Gizatullina <elvirawed@gmail.com>
2023-03-24 15:23:34 -0700

    Sushi Gallery container with styling

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,30 +1,40 @@
-import bg from '../assets/bg.png';
-import chef from '../assets/chef.png';
-import G from '../assets/G.png';
-import gallery01 from '../assets/gallery01.png';
-import gallery02 from '../assets/gallery02.png';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
-import knife from '../assets/knife.png';
-import logo from '../assets/logo.png';
-import menu from '../assets/menu.png';
-import overlaybg from '../assets/overlaybg.png';
-import spoon from '../assets/spoon.svg';
-import welcome from '../assets/welcome.png';
-import findus from '../assets/findus.png';
-import laurels from '../assets/laurels.png';
-import award01 from '../assets/award01.png';
-import award02 from '../assets/award02.png';
-import award03 from '../assets/award03.png';
-import award05 from '../assets/award05.png';
-import sign from '../assets/sign.png';
-import quote from '../assets/quote.png';
-import gericht from '../assets/gericht.png';
+import bg from "../assets/bg.png";
+import chef from "../assets/chef.png";
+import G from "../assets/G.png";
+import A from "../assets/A.png";
+import gallery01 from "../assets/gallery01.png";
+import gallery02 from "../assets/gallery02.png";
+import gallery03 from "../assets/gallery03.png";
+import gallery04 from "../assets/gallery04.png";
+import knife from "../assets/knife.png";
+import logo from "../assets/logo.png";
+import menu from "../assets/menu.png";
+import overlaybg from "../assets/overlaybg.png";
+import spoon from "../assets/spoon.svg";
+import welcome from "../assets/welcome.png";
+import findus from "../assets/findus.png";
+import laurels from "../assets/laurels.png";
+import award01 from "../assets/award01.png";
+import award02 from "../assets/award02.png";
+import award03 from "../assets/award03.png";
+import award05 from "../assets/award05.png";
+import sign from "../assets/sign.png";
+import quote from "../assets/quote.png";
+import gericht from "../assets/gericht.png";
+import sushi01 from "../assets/sushi01.jpg";
+import sushi02 from "../assets/sushi02.jpg";
+import sushi03 from "../assets/sushi03.jpg";
+import sushi04 from "../assets/sushi04.jpg";
 
 export default {
   bg,
   chef,
   G,
+  A,
+  sushi01,
+  sushi02,
+  sushi03,
+  sushi04,
   gallery01,
   gallery02,
   gallery03,
--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import React from "react";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
 
-import './Gallery.css';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Gallery.css";
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  const scrollRef = React.useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === "left") {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Sushi Gallery</h1>
+        <p
+          className="p__opensans"
+          style={{ color: "#AAAAAA", marginTop: "2rem" }}
+        >
+          Serving up the best sushi in San Diego using the freshest ingredients.
+          Join us for specials and your favorite sushi.
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {[images.sushi01, images.sushi02, images.sushi03, images.sushi04].map(
+            (image, index) => (
+              <div
+                className="app__gallery-images_card flex__center"
+                key={`gallery_image-${index + 1}`}
+              >
+                <img src={image} alt="gallery_image" />
+                <BsInstagram className="gallery__image-icon" />
+              </div>
+            )
+          )}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("left")}
+          />
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("right")}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Gallery;
