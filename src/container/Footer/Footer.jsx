4578214290e487d7450637069193a764fbd083a1
Elvira Gizatullina <elvirawed@gmail.com>
2023-03-26 11:59:57 -0700

    Footer container without styling

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -9,6 +9,44 @@ const Footer = () => (
   <div className="app__footer section__padding" id="login">
     <FooterOverlay />
     <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">2321 5th Ave San Diego, CA 92101</p>
+        <p className="p__opensans">(619) 238-4760</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">
+          &quot;Azuki Sushi offers contemporary mix of sushi along with a
+          variety of eclectic seafood items.&quot;
+        </p>
+        <img
+          src={images.spoon}
+          className="spoon__img"
+          style={{ marginTop: 15 }}
+        />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">04:00 pm - 10:00 pm</p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">04:00 pm - 11:00 pm</p>
+      </div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2023 Azuki. All Rights reserved.</p>
+    </div>
   </div>
 );
 
